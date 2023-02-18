c1c769d2165cd6afb9c26b7a52543cc87af849d4
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-02-18 09:29:19 -0300

    Dummy implementation for contFracFromNum
    - Create file contFrac.c
    - Create auxiliary function to allocate memory for the contFrac* data
      type
    - Create function to free memory allocated for contFrac*
    - Implement test version of contFracFromNum: just add the string
      representation for now
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

---
+++ contFrac.c
@@ -0,0 +1,33 @@
+#include "contFrac.h"
+
+contFrac* allocContFrac(unsigned short int numberOfSteps) {
+	int numberOfChars = 2 * (numberOfSteps + 1);
+
+	contFrac *fraction = malloc(sizeof(contFrac));
+	fraction->numbers = malloc(numberOfSteps * sizeof(unsigned int));
+	fraction->representation = malloc(numberOfChars * sizeof(char));
+
+	return fraction;
+}
+
+void freeContFrac(contFrac *fraction) {
+	free(fraction->numbers);
+	free(fraction->representation);
+	free(fraction);
+}
+
+contFrac* contFracFromNum(float realNumber) {
+	contFrac *fraction = allocContFrac(MAX_STEPS / 2);
+	sprintf(fraction->representation, "%f", realNumber);
+	return fraction;
+}
+
+contFrac* contFracFromStr(char* representation);
+contFrac* contFracFromFrac(int numerator, unsigned int denominator);
+
+float realNumber(contFrac *fraction);
+char* representation(contFrac *fraction);
+
+contFrac* minus(contFrac *fraction);
+contFrac* add(contFrac *first, contFrac *second);
+contFrac* subtract(contFrac *minuend, contFrac *subtrahend);
