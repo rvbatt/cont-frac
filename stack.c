362f2ff4846eef0fa7a6ae7aaef8c69451852a4b
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-02-22 14:51:30 -0300

    Fix memory leak using pointer to Stack
    - Instead of using a Stack, use a Stack* (pointer) to keep a reference
      to the allocated memory
    - Stop initializing the first node with 0
    -
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- stack.c
+++ stack.c
@@ -1,19 +1,13 @@
 #include "stack.h"
 
-Stack createStack() {
-	Stack *stack = malloc(sizeof(Stack));
-
-	stack->top = malloc(sizeof(Node));
-	stack->top->data = 0;
-	stack->top->next = NULL;
-
-	stack->size = malloc(sizeof(unsigned));
-	*(stack->size) = 0;
-
-	return *stack;
+Stack* createStack() {
+	Stack* stack = malloc(sizeof(Stack));
+	stack->top = NULL;
+	stack->size = 0;
+	return stack;
 }
 
-void freeNodes(Node *node) {
+void freeNodes(Node* node) {
 	if (node == NULL) return;
 
 	freeNodes(node->next);
@@ -21,33 +15,30 @@ void freeNodes(Node *node) {
 	free(node);
 }
 
-void freeStack(Stack stack) {
-	freeNodes(stack.top);
-	free(stack.size);
+void freeStack(Stack* stack) {
+	freeNodes(stack->top);
 	free(stack);
 }
 
-void push(Stack stack, Item item) {
-	Node *newNode = malloc(sizeof(Node));
-	newNode->data = item;
-	newNode->next = stack.top;
+void push(Stack* stack, Item item) {
+	Node* newNode = malloc(sizeof(Node));
+	newNode->data = malloc(sizeof(Item));
+	*(newNode->data) = item;
+	newNode->next = stack->top;
 
-	stack.top = newNode;
-	*(stack.size)++;
+	stack->top = newNode;
+	(stack->size)++;
 }
 
-Item pop(Stack stack) {
-	Item popedItem = stack.top->data;
-	Node *nextNode = stack.top->next;
+Item pop(Stack* stack) {
+	Item popedItem = *(stack->top->data);
+	Node* nextNode = stack->top->next;
 
-	free(stack.top->data);
-	free(stack.top);
+	free(stack->top->data);
+	free(stack->top);
 
-	stack.top = nextNode;
+	stack->top = nextNode;
+	(stack->size)--;
 	return popedItem;
-}
-
-unsigned sizeOfStack(Stack stack) {
-	return *(stack.size);
 }
 
